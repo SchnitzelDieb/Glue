35c169614d854816e305c51a99012f58876af2fe
SchnitzelDieb <jf1407@gmx.de>
2025-12-13 19:54:48 +0100

    Resource manager implementiert

---
+++ gamejam-spiel-2025-12-13-15-33-43/Scripts/resource_manager.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var fuel: int = 100
+var max_fuel: int = 100
+
+var can_kill: bool = true
+
+var crewmate_killed: bool = false
+var imposter_killed: bool = true
+
+var imposter_count: int = 0
+var crewmate_count: int = 0
+
+var ship_repair: int = 0
+
+#Handle Imposter threats
+func night() -> void:
+	fuel -= 20
+	var i: int = imposter_count
+	while i > 0:
+			max_fuel -= 10
+			crewmate_count -= 1
+			if crewmate_count <= 0:
+				pass #Endscreen(Lose) hier aufrufen
+			i = -1
+	ship_repair += crewmate_count
+	if ship_repair >= 6:
+		pass #Endscreen(Win) hier aufrufen
+	pass
