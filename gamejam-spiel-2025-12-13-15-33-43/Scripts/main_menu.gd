763af2d54ec2ece15096904e14b87239a48cfe5d
SchnitzelDieb <jf1407@gmx.de>
2025-12-14 00:08:34 +0100

    Credits Scene Adden + Resourcemanager fertigstellen

--- gamejam-spiel-2025-12-13-15-33-43/Scripts/main_menu.gd
+++ gamejam-spiel-2025-12-13-15-33-43/Scripts/main_menu.gd
@@ -6,3 +6,7 @@ func _on_start_game_pressed() -> void:
 
 func _on_close_game_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
